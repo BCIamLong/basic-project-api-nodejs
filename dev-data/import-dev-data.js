c40a4eef45385dac7d4ca4f288c1a186487e594b
longhoang <[hoalanh21112002@gmail.com]>
2023-08-09 13:56:45 +0700

    Modeling data and create import-dev-data file support

---
+++ dev-data/import-dev-data.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const Post = require('../models/postModel');
+const User = require('../models/userModel');
+
+dotenv.config({ path: './config.env' });
+
+const posts = JSON.parse(fs.readFileSync(`${__dirname}/posts.json`));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`));
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() => console.log('DB connect successful'))
+  .catch((err) => console.log(err));
+
+const importData = async (model, data) => {
+  try {
+    await model.insertMany(data);
+    console.log('Import data success');
+  } catch (err) {
+    console.log(err);
+  }
+  // mongoose.connection.close();
+  //!! process.exit() is the way not good to stop process but in this case we only interact with DB and some files so it's not problem but in project we shouldn't use this
+  process.exit();
+};
+
+const deleteAllData = async (model) => {
+  try {
+    await model.deleteMany();
+    console.log('Delete all data success');
+  } catch (err) {
+    console.log(err);
+  }
+  // mongoose.connection.close();
+  process.exit();
+};
+
+// console.log(process.argv);
+// importData(posts);
+// deleteAllData();
+if (process.argv[2] === '--import-post') importData(Post, posts);
+if (process.argv[2] === '--delete-post') deleteAllData(Post);
+if (process.argv[2] === '--import-user') importData(User, users);
+if (process.argv[2] === '--delete-user') deleteAllData(User);
--- models/userModel.js
+++ models/userModel.js
@@ -1,25 +1,45 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Name not empty"],
+    required: [true, 'Name not empty'],
   },
-  userName: {
+  username: {
     type: String,
-    required: [true, "User not empty"],
+    required: [true, 'User not empty'],
     unique: true,
   },
   email: {
     type: String,
+    required: [true, 'User must have an email'],
     unique: true,
   },
+  address: {
+    street: String,
+    suite: String,
+    city: String,
+    zipcode: String,
+    geo: {
+      lat: String,
+      lng: String,
+    },
+  },
   phone: {
-    type: Number,
+    type: String,
+    required: [true, 'User must have a phone number'],
     unique: true,
+  },
+  website: {
+    type: String,
+  },
+  company: {
+    name: String,
+    catchPhrase: String,
+    bs: String,
   },
 });
 
-const User = new mongoose.model("User", userSchema);
+const User = new mongoose.model('User', userSchema);
 
 module.exports = User;
