23d4032da006b9096b29e8a2499af528b09018c3
longhoang <[hoalanh21112002@gmail.com]>
2023-08-09 13:56:45 +0700

    Modeling data and create import-dev-data file support

--- models/postModel.js
+++ models/postModel.js
@@ -1,19 +1,37 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: [true, "Title not empty"],
+    required: [true, 'A post mush have a title'],
   },
-  body: {
+  content: {
     type: String,
+    required: [true, 'A post must have a content'],
   },
   author: {
     type: String,
-    unique: true,
+    // unique: true,
+  },
+  image: {
+    type: String,
+    required: [true, 'Post must have an image'],
+  },
+  images: [String],
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  comment: {
+    type: String,
+    default: '',
+  },
+  shares: {
+    type: Number,
+    default: 0,
   },
 });
 
-const Post = new mongoose.model("Post", postSchema);
+const Post = new mongoose.model('Post', postSchema);
 
 module.exports = Post;
